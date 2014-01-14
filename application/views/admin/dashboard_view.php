4853a518efbd9e89f00e92f42fd35a24078f5a68
fanat98 <a.idrisov@bytheway.ch>
2014-01-14 22:56:58 +0100

    home_teil2.1

--- application/views/admin/dashboard_view.php
+++ application/views/admin/dashboard_view.php
@@ -1,33 +1,35 @@
-<div class="pagination">
-        <ul>
-<?php echo $this->pagination->create_links();?>
-        </ul>  </div>
-    <table class="table table-hover">
-        <thead>
 
+    <div class="pagination">
+        <ul>
+            <?php echo $this->pagination->create_links();?>
+        </ul>  
+    </div>
+    
+    <table class="table table-hover">
         
-   <tr>
-        <?php foreach($fields as $field_name => $field_display): ?>
-        <th <?php if ($sort_by == $field_name) echo "class=\"sort_$sort_order\"" ?>>
-        <?php echo anchor("dashboard/index/$field_name/" . (($sort_order == 'asc' && $sort_by == $field_name) ? 'desc' : 'asc') , 
-        $field_display); ?> </th> <?php endforeach; ?>
-  </tr>
+      <thead> 
+        <tr>
+            <?php foreach($fields as $field_name => $field_display): ?>
+            <th <?php if ($sort_by == $field_name) echo "class=\"sort_$sort_order\"" ?>>
+            <?php echo anchor("dashboard/index/$field_name/" . (($sort_order == 'asc' && $sort_by == $field_name) ? 'desc' : 'asc') , 
+            $field_display); ?> </th> <?php endforeach; ?>
+        </tr>
         </thead>
+        
         <tbody>
         <?php foreach ($items as $item): ?>
-        <tr>
             
-        
-        <?php foreach ($fields as $field_name => $field_display):?>
-        
+            <tr>
+                
+              <?php foreach ($fields as $field_name => $field_display):?>
+                
                 <td><?php echo $item->$field_name;?></td>
         
-        <?php endforeach; ?>
-    
- 
+              <?php endforeach; ?>
+                
+            </tr>
+            
+             <?php endforeach; ?>
 
-        </tr>
-        <?php endforeach; ?>
-
-        </tbody>
+       </tbody>
     </table>
