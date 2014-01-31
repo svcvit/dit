d64a3184e7526fd41814d6001b61beac22c37dd6
fanat98 <a.idrisov@bytheway.ch>
2014-01-31 20:25:43 +0100

    Update database.php
    db

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'ueli-_skinv-prod';
+$db['default']['username'] = '';
 $db['default']['password'] = '';
-$db['default']['database'] = 'ueli-banholzer_swarovski-invitation-prod';
+$db['default']['database'] = 'd';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
